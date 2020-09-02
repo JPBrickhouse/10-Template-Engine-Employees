d1059afdefbab3b3474209abe3a8a268c4ea8f15
JBrickhouse <jbrickhouse777@gmail.com>
2020-09-02 12:27:01 -0500

    Writing the arrays of the objects containing the questions

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
+const OUTPUT_DIR = path.resolve(__dirname, "./output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
@@ -27,6 +27,73 @@ const render = require("./lib/htmlRenderer");
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
+
+const employeeQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?",
+        type: "string"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the employee's id number?",
+        type: "number"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the employee's email address?",
+        // Validation code sourced from the following: https://gist.github.com/Amitabh-K/ae073eea3d5207efaddffde19b1618e8
+        validate: function (email) {
+            var valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+            if (valid) {
+                return true;
+            } else {
+                console.log("\nPlease enter a valid email")
+                return false;
+            }
+        }
+    },
+    {
+        type: "list",
+        name: "employeeType",
+        message: "This person is what type of employee?",
+        choices: [
+            "Manager",
+            "Intern",
+            "Engineer"
+        ]
+    }
+]
+
+const managerQuestions = [    
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office number?",
+        type: "number"
+    }
+]
+
+const internQuestions = [
+    {
+        type: "input",
+        name: "school",
+        message: "What school does this student attend?",
+        type: "string"
+    }
+]
+
+const engineerQuestions = [
+    {
+        type: "input",
+        name: "github",
+        message: "Please enter your GitHub username (not the url, just the username):",
+        type:"string"
+    }
+]
 
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
